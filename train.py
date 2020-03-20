06c0def2a08b587fe59d9fbc01efc89314d72e75
Paul Francoeur <33321268+francoep@users.noreply.github.com>
2020-03-20 13:53:48 -0400

    Update train.py
    adding hyperparams for optimizers

--- train.py
+++ train.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-#TODO: clean the script from changing into the src directory and having everything being mapped from there.
-
 import os
 import sys
 import pandas as pd
@@ -10,12 +8,10 @@ import torch
 import numpy as np
 #from rdkit.Chem import AllChem as Chem
 
-#TODO -- change this behavior
-#os.chdir('src')
-print(os.getcwd())
+#we assume that you are running the model from the main section of this github repository
 sys.path.append(os.getcwd())
 sys.path.append('src')
-print(sys.path)
+
 import argparse
 #import matplotlib.pyplot as plt  removing the plotting behavior -- instead just saving out the data
 from featurization.data_utils import load_data_from_df, construct_loader
@@ -36,18 +32,26 @@ parser=argparse.ArgumentParser(description='Train and test MAT model on datasets
 parser.add_argument('--train',type=str,required=True,help='Train data filename. Assumed to be a csv: smile,y')
 parser.add_argument('--test',type=str,required=True,help='Test data filename. Assumed to be a csv: smile,y')
 parser.add_argument('--pretrain',action='store_true',help='Flag to use the pretrained weights. If set, will use. Assumed to be pretrained_weights.pt')
-parser.add_argument('--datadir',type=str,required=True,help='Absolutepath to the directory for the data from training and testing the model. Saved filenames will be <train>_<loss>_<optim>_<optimHP>_<epochs>_<train|test|testmetrics>.pi')
-parser.add_argument('--savemodel', action='store_true',help='Flag to save the trained model. The filename will be <traindata>_trained.model')
+parser.add_argument('--datadir',type=str,required=True,help='Absolutepath to the directory for the data from training and testing the model. Saved filenames will be <prefix>_<trainlosses|trainepochlosses|testdic>.pi')
+parser.add_argument('--savemodel', action='store_true',help='Flag to save the trained model. The filename will be <prefix>_trained.model')
 parser.add_argument('-e','--epochs', type=int, default=500,help='Number of epochs to train the model for. Defaults to 500')
-parser.add_argument('-l','--loss',type=str,default='mse',help='Loss Function to use: mse, mae, huber, or logcosh')
-parser.add_argument('-o','--optimizer',type=str,default='sgd',help='Optimizer for training the model: sgd, or adam')
+parser.add_argument('-l','--loss',type=str,default='mse',help='Loss Function to use: mse, mae, huber, or logcosh.')
+parser.add_argument('-o','--optimizer',type=str,default='sgd',help='Optimizer for training the model: sgd, or adam.')
+parser.add_argument('-p','--prefix',type=str,required=True,help='Prefix for the output files for training.')
+parser.add_argument('--lr',type=float,default=1e-4, help='Learning rate for the Optimizer. Defaults to 1e-4.')
+parser.add_argument('--momentum',type=float,default=0.9,help='Momentum for SGD optimizer. Defaults to 0.9')
+parser.add_argument('--weight_decay',type=float,default=0,help='L2 pentalty for Optimizer. Defaults to 0')
+parser.add_argument('--dampening',type=float,default=0,help='Dampening for momemtum in SGD Optimizer. Defaults to 0')
+parser.add_argument('--nesterov',action='store_true',default=False,help='Enable Nesterov momentum for SGD.')
+parser.add_argument('--beta1',type=float,default=0.9,help='Beta1 for ADAM optimizer. Defaults to 0.9')
+parser.add_argument('--beta2',type=float,default=0.999,help='Beta2 for ADAM optimizer. Defaults to 0.999')
+parser.add_argument('--epsilon',type=float,default=1e-08,help='Epsilon for ADAM optimizer. Defaults to 1e-08.')
+parser.add_argument('--amsgrad',action='store_true',default=False,help='Enables AMSGrad varient of ADAM.')
 
 args=parser.parse_args()
 
-#TODO -- implement 'quantile' loss funtion
+#TODO -- implement 'quantile' loss funtion?
 assert args.loss in set(['mse','mae','huber','logcosh']) and args.optimizer in set(['sgd','adam'])
-
-trainname_for_output=args.train.split('/')[-1].replace('.csv','_')
 
 #todo -- implement changing hyper params -- Sections to support indicated by a comment of #1#
 
@@ -91,20 +95,20 @@ model.cuda()
 
 #TODO -- extra loss functions -- quantile?
 if args.loss == 'mse':
-    criterion=torch.nn.MSELoss(reduction='sum')
+    criterion=torch.nn.MSELoss(reduction='mean')
 elif args.loss=='mae':
-    criterion=torch.nn.L1Loss(reduction='sum')
+    criterion=torch.nn.L1Loss(reduction='mean')
 elif args.loss=='huber':
-    criterion=torch.nn.SmoothL1Loss(reduction='sum')
+    criterion=torch.nn.SmoothL1Loss(reduction='mean')
 elif args.loss=='logcosh':
     criterion=None
 
 
-#TODO -- extra optimizers -- SGD (lr, momemtum,weight_decay), ADAM(lr, beta1, beta2, eps, weight_decay)  & implement their hyper-params into running.
+#Selecting Optimizer
 if args.optimizer=='sgd':
-    optimizer=torch.optim.SGD(model.parameters(),lr=1e-4,momentum=0.9,weight_decay=0,dampening=0,nesterov=False)
+    optimizer=torch.optim.SGD(model.parameters(),lr=args.lr,momentum=args.momemtum,weight_decay=args.weight_decay,dampening=args.dampening,nesterov=args.nesterov)
 elif args.optimizer=='adam':
-    optimizer=torch.optim.Adam(model.parameters(),lr=0.001,betas=(0.9,0.999),eps=1e-08,weight_decay=0,amsgrad=False)
+    optimizer=torch.optim.Adam(model.parameters(),lr=args.lr,betas=(args.beta1,args.beta2),eps=args.epsilon,weight_decay=args.weight_decay,amsgrad=args.amsgrad)#default adam has lr=0.001
 
 losses=[]
 
@@ -128,29 +132,29 @@ for epoch in range(args.epochs):
 
 #saving the trained model
 if args.savemodel:
-    torch.save(model.state_dict(),trainname_for_output+args.optimizer+'_trained.model')
+    torch.save(model.state_dict(),args.prefix+'_trained.model')
 
 #now that the training is complete -- we need to output the training losses
 epoch_mean_losses=[]
 for i in range(int(len(losses)/len(data_loader))):
     tmp=[]
     for j in range(int(len(data_loader))):
-        tmp.append(losses[int(j+i*len(losses)/len(data_loader))])
+        tmp.append(losses[int(j+i*len(losses))])
     epoch_mean_losses.append(np.mean(tmp))
 
 #making the directory for the data
 if not os.path.isdir(args.datadir):
     os.mkdir(args.datadir)
 
-#saving the training loss -- <train>_<loss>_<optim>_<optimHP>_<epochs>_<train|test|testmetrics>.pi
-#TODO -- adding the hyper parameters for the optimizer
-with open(args.datadir+'/'+trainname_for_output+args.optimizer+'_'+str(args.epochs)+'_trainloss.pi','wb') as outfile:
+#saving the training losses
+with open(args.datadir+'/'+args.prefix+'_trainlosses.pi','wb') as outfile:
+    pickle.dump(losses,outfile)
+with open(args.datadir+'/'+args.prefix+'_trainepochlosses.pi','wb') as outfile:
     pickle.dump(epoch_mean_losses,outfile)
 
 #we need to evaluate the test_set
 testX, testy=load_data_from_df(args.test,one_hot_formal_charge=True)
 testdata_loader=construct_loader(testX,testy,batch_size)
-
 gold=np.array([])
 preds=np.array([])
 model.eval()
@@ -162,12 +166,15 @@ for batch in testdata_loader:
     y_pred = model(node_features, batch_mask, adjacency_matrix, distance_matrix, None)
     preds=np.append(preds,y_pred.tolist())
 
-with open(args.datadir+'/'+trainname_for_output+args.optimizer+'_'+str(args.epochs)+'_test.pi','wb') as outfile:
-    pickle.dump((gold,preds),outfile)
-
 r2=np.corrcoef(preds,gold)[0][1]**2
 rmse=np.sqrt(np.mean(preds-gold)**2)
 
-with open(args.datadir+'/'+trainname_for_output+args.optimizer+'_'+str(args.epochs)+'_testmetrics.txt','w') as outfile:
-    outfile.write(f'R2: {r2}\n')
-    outfile.write(f'RMSE: {rmse}\n')
+testdic={
+    'predicted':preds,
+    'target':gold,
+    'RMSE':rmse,
+    'R2':r2
+}
+
+with open(args.datadir+'/'+args.prefix+'_testdic.pi','wb') as outfile:
+    pickle.dump(testdic,outfile)
