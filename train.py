e7314ec9cb0c7c5351722b03a6d4ef4e8e95f51f
Paul Francoeur <33321268+francoep@users.noreply.github.com>
2020-05-13 11:55:28 -0400

    Update train.py
    Adding in Parameter counting, timing functionality, and making utilizing wandb a selectable option

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import torch
 #from tqdm import tqdm
 import numpy as np
 import wandb
+import time
 
 #we assume that you are running the model from the main section of this github repository
 sys.path.append(os.getcwd())
@@ -14,7 +15,6 @@ sys.path.append('src')
 
 import argparse
 #import matplotlib.pyplot as plt  removing the plotting behavior -- instead just saving out the data
-from featurization.data_utils import load_data_from_df, construct_loader
 from transformer import make_model
 import pickle
 
@@ -25,7 +25,7 @@ parser.add_argument('--fold',type=str,required=True,help='Fold for the datafiles
 parser.add_argument('--pretrain',action='store_true',default=False,help='Flag to use the pretrained weights. If set, will use. Assumed to be pretrained_weights.pt')
 parser.add_argument('--datadir',type=str,default='sweep',help='Absolutepath to the directory for the data from training and testing the model (Def: sweep). Saved filenames will be <prefix>_<fold>_e<epochs>_<loss>_<optimizer>_lr<lr>_m<momentum>_wd<weightdecay>_<trainlosses|trainepochlosses|testdic>.pi')
 parser.add_argument('--savemodel', action='store_true',default=False,help='Flag to save the trained model. The filename will be <prefix>_trained.model')
-#parser.add_argument('--only2d',action='store_true',default=False,help='Flag to only use 2D conformers for making the distance matrix.')
+parser.add_argument('--only2d',action='store_true',default=False,help='Flag to only use 2D conformers for making the distance matrix.')
 parser.add_argument('-e','--epochs', type=int, default=500,help='Number of epochs to train the model for. Defaults to 500')
 parser.add_argument('-l','--loss',type=str,default='mse',help='Loss Function to use: mse, mae, huber, or logcosh.')
 parser.add_argument('-o','--optimizer',type=str,default='sgd',help='Optimizer for training the model: sgd, or adam.')
@@ -33,7 +33,6 @@ parser.add_argument('--lr',type=float,default=1e-4, help='Learning rate for the 
 parser.add_argument('--momentum',type=float,default=0.9,help='Momentum for SGD optimizer. Defaults to 0.9')
 parser.add_argument('--weight_decay',type=float,default=0,help='L2 pentalty for Optimizer. Defaults to 0')
 parser.add_argument('--dampening',type=float,default=0,help='Dampening for momentum in SGD Optimizer. Defaults to 0')
-#parser.add_argument('--nesterov',action='store_true',default=False,help='Enable Nesterov momentum for SGD.')
 parser.add_argument('--beta1',type=float,default=0.9,help='Beta1 for ADAM optimizer. Defaults to 0.9')
 parser.add_argument('--beta2',type=float,default=0.999,help='Beta2 for ADAM optimizer. Defaults to 0.999')
 parser.add_argument('--epsilon',type=float,default=1e-08,help='Epsilon for ADAM optimizer. Defaults to 1e-08.')
@@ -44,24 +43,24 @@ parser.add_argument('--Ndense',type=int,default=1,help='Number of Dense blocks i
 parser.add_argument('--heads',type=int,default=16,help='Number of attention heads in MultiHeaded Attention. **Needs to evenly divide dmodel Defaults to 16.')
 parser.add_argument('--dmodel',type=int,default=1024,help='Dimension of the hidden layer for the model. Defaults to 1024.')
 parser.add_argument('--nstacklayers',type=int,default=8,help='Number of stacks in the Encoder layer. Defaults to 8')
-#parser.add_argument('--amsgrad',action='store_true',default=False,help='Enables AMSGrad varient of ADAM.')
+parser.add_argument('--cpu',action='store_true',default=False,help='Flag to have model be CPU only.')
+parser.add_argument('--wandb',action='store_true',default=False,help='Flag if using Weights and Biases to log.')
 
 args=parser.parse_args()
+
+if args.cpu:
+    from featurization.cpu_data_utils import load_data_from_df, construct_loader
+else:
+    from featurization.data_utils import load_data_from_df, construct_loader
 
 namep=args.prefix.split('/')[-1]
 outf_prefix=f'{namep}_{args.fold}_drop{args.dropout}_ldist{args.ldist}_lattn{args.lattn}_Ndense{args.Ndense}_heads{args.heads}_dmodel{args.dmodel}_nsl{args.nstacklayers}'
 
 #wandb things
-wandb.init(project='MAT',name=outf_prefix)
+if args.wandb:
+    wandb.init(project='MAT',name=outf_prefix)
 
-#TODO -- implement 'quantile' loss funtion?
 assert args.loss in set(['mse','mae','huber','logcosh']) and args.optimizer in set(['sgd','adam'])
-
-#checking if we need 2donly things
-if '_2d' in args.prefix:
-    need2d=True
-else:
-    need2d=False
 
 trainfile=args.prefix+'_train'+args.fold+'.csv'
 testfile=args.prefix+'_test'+args.fold+'.csv'
@@ -69,17 +68,17 @@ print('Trainfile:',trainfile)
 print('Testfile:',testfile)
 print('Outfile Prefix:',outf_prefix)
 print('Loading train and test data')
+
 #loading the training & testing data
 batch_size=8
-trainX, trainy=load_data_from_df(trainfile,one_hot_formal_charge=True,two_d_only=need2d)
+trainX, trainy=load_data_from_df(trainfile,one_hot_formal_charge=True,two_d_only=args.only2d)
 data_loader=construct_loader(trainX,trainy,batch_size)
-testX, testy=load_data_from_df(testfile,one_hot_formal_charge=True,two_d_only=need2d)
+testX, testy=load_data_from_df(testfile,one_hot_formal_charge=True,two_d_only=args.only2d)
 testdata_loader=construct_loader(testX,testy,batch_size)
 
 
 d_atom = trainX[0][0].shape[1]
 
-# Since we are using the provided pretrained weights -- these are locked in place
 model_params= {
     'd_atom': d_atom,
     'd_model': args.dmodel,
@@ -96,9 +95,13 @@ model_params= {
 }
 
 print('Making Model')
-model=make_model(**model_params)
 
-wandb.watch(model,'all')
+model=make_model(**model_params)
+param_count=sum(p.numel() for p in model.parameters() if p.requires_grad)
+print('Number of parameters:',param_count)
+
+if args.wandb:
+    wandb.watch(model,'all')
 
 if args.pretrain:
     print('Using Pretrained Weights')
@@ -112,8 +115,11 @@ if args.pretrain:
             param = param.data
         model_state_dict[name].copy_(param)
 
-torch.cuda.empty_cache()
-model.cuda()
+if args.cpu:
+    model.cpu()
+else:
+    torch.cuda.empty_cache()
+    model.cuda()
 model.train()
 
 #TODO -- extra loss functions -- quantile?
@@ -156,13 +162,15 @@ for epoch in range(args.epochs):
         else:
             loss=criterion(y_pred,y)
         losses.append(loss.item())
-        wandb.log({"Train Loss":loss.item()},step=iteration)
+        if args.wandb:
+            wandb.log({"Train Loss":loss.item()},step=iteration)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        torch.cuda.empty_cache()
+        if not args.cpu:
+            torch.cuda.empty_cache()
 
         if iteration%1000==0:
             #we evaluate the test set.
@@ -176,18 +184,21 @@ for epoch in range(args.epochs):
                 t_batch_mask = torch.sum(torch.abs(t_node_features), dim=-1) != 0
                 t_y_pred = model(t_node_features, t_batch_mask, t_adjacency_matrix, t_distance_matrix, None)
                 preds=np.append(preds,t_y_pred.tolist())
-                torch.cuda.empty_cache()
+                if not args.cpu:
+                    torch.cuda.empty_cache()
 
             test_rmse=np.sqrt(np.mean((preds-gold)**2))
             test_r2=np.corrcoef(preds,gold)[0][1]**2
             model.train()
-            wandb.log({"Test RMSE":test_rmse,"Test R2":test_r2},step=iteration)
+            if args.wandb:
+                wandb.log({"Test RMSE":test_rmse,"Test R2":test_r2},step=iteration)
 
     #end of 1 epoch -- time to log the stats
     train_rmse, train_r2=(np.sqrt(np.mean(epoch_preds-epoch_gold)**2), np.corrcoef(epoch_preds,epoch_gold)[0][1]**2)
-    wandb.log({"Train Epoch RMSE":train_rmse,"Train Epoch R2":train_r2},step=iteration)
+    if args.wandb:
+        wandb.log({"Train Epoch RMSE":train_rmse,"Train Epoch R2":train_r2},step=iteration)
 
-
+print('Training Complete!')
 #saving the trained model
 if args.savemodel:
     torch.save(model.state_dict(),args.prefix+'_trained.model')
@@ -211,33 +222,58 @@ with open(args.datadir+'/'+outf_prefix+'_trainepochlosses.pi','wb') as outfile:
     pickle.dump(epoch_mean_losses,outfile)
 
 #final evaluations
+print('Final Evaluations!')
+print('Training Set:')
 model.eval()
 
 #first the training set
+#TODO -- Implement timing here -- 
 gold=np.array([])
 preds=np.array([])
+t0=time.time()
+train_times=[]
 for batch in data_loader:
+    t1=time.time()
     adjacency_matrix, node_features, distance_matrix, y = batch
+    tload=time.time()-t1
     gold=np.append(gold,y.tolist())
     batch_mask = torch.sum(torch.abs(node_features), dim=-1) != 0
     y_pred = model(node_features, batch_mask, adjacency_matrix, distance_matrix, None)
+    tpred=time.time()-t1
     preds=np.append(preds,y_pred.tolist())
-    torch.cuda.empty_cache()
+    if not args.cpu:
+        torch.cuda.empty_cache()
+
+    train_times.append((tload,tpred))
+
+print('Overall Time: ',time.time()-t0)
 
 r2=np.corrcoef(preds,gold)[0][1]**2
 rmse=np.sqrt(np.mean((preds-gold)**2))
-wandb.log({"Train Epoch RMSE":rmse,"Train Epoch R2":r2},step=iteration+1)
+if args.wandb:
+    wandb.log({"Train Epoch RMSE":rmse,"Train Epoch R2":r2},step=iteration+1)
 
 #we need to evaluate the test_set
+print('Test Set:')
 gold=np.array([])
 preds=np.array([])
+t0=time.time()
+test_times=[]
 for batch in testdata_loader:
+    t1=time.time()
     adjacency_matrix, node_features, distance_matrix, y = batch
+    tload=time.time()-t1
     gold=np.append(gold,y.tolist())
     batch_mask = torch.sum(torch.abs(node_features), dim=-1) != 0
     y_pred = model(node_features, batch_mask, adjacency_matrix, distance_matrix, None)
+    tpred=time.time()-t1
     preds=np.append(preds,y_pred.tolist())
-    torch.cuda.empty_cache()
+    if not args.cpu:
+        torch.cuda.empty_cache()
+
+    test_times.append((tload,tpred))
+
+print('Overall Time: ',time.time()-t0)
 
 r2=np.corrcoef(preds,gold)[0][1]**2
 rmse=np.sqrt(np.mean((preds-gold)**2))
@@ -246,10 +282,13 @@ testdic={
     'predicted':preds,
     'target':gold,
     'RMSE':rmse,
-    'R2':r2
+    'R2':r2,
+    'Traintimes':train_times,
+    'Testtimes':test_times
 }
 
-wandb.log({"Test RMSE":rmse,"Test R2":r2},step=iteration+1)
+if args.wandb:
+    wandb.log({"Test RMSE":rmse,"Test R2":r2},step=iteration+1)
 
 with open(args.datadir+'/'+outf_prefix+'_testdic.pi','wb') as outfile:
     pickle.dump(testdic,outfile)
